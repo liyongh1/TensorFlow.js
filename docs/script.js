c8b7484a71a96381134cbdba35f5d5ebaf8533d3
Tyler <tyler.li@mail.utoronto.ca>
2019-12-03 23:52:43 -0500

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,2 +1,17 @@
 
 console.log('Hello TensorFlow');
+
+/**
+* Get the car data reduced to just the variables we are interested
+* and cleaned of missing data.
+*/
+async function getData() {
+        const carsDataReq = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json'); 
+        const carsData = await carsDataReq.json(); 
+        const cleaned = carsData.map(car => ({
+                                               mpg: car.Miles_per_Gallon,
+                                               horsepower: car.Horsepower,
+                                              }))
+       .filter(car => (car.mpg != null && car.horsepower != null));
+       return cleaned;
+}
